46798012f75b9944c05b81c3479da886e1ef6fd5
Steven Rayesky <protolexus@gmail.com>
2018-06-21 08:52:03 -0400

    added formatSession to helpers.js to capitalize and remove whitespace from sessionId.

---
+++ src/helpers.js
@@ -0,0 +1,6 @@
+export function formatSession(str) {
+    let sessionId = str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+    return sessionId.replace(/\s+/g, '');
+}
