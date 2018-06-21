3504e90307f75ffbee81123c5c6dd440cfc4602a
Steven Rayesky <protolexus@gmail.com>
2018-06-21 08:59:04 -0400

    setup form submit and route to new session.

---
+++ src/components/SessionPicker/SessionPicker.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { formatSession } from '../../helpers';
+
+class SessionPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {inputValue: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({inputValue: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const sessionId = formatSession(this.state.inputValue);
+        console.log(sessionId);
+        this.props.history.push(`/session/${sessionId}`);
+    }
+
+    render() {
+        return (
+            <form className="session-picker" onSubmit={this.handleSubmit}>
+                <h2>type in your session name to go to it</h2>
+                <input type="text" required placeholder="Session Name" value={this.state.inputValue} onChange={this.handleChange}/>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+
+
+export default SessionPicker;
