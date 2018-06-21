8cb87218f26f2ac5205ce7db10d36a59773dfa69
Steven Rayesky <protolexus@gmail.com>
2018-06-21 13:35:49 -0400

    added the two players to Game component in head-to-head. Added update player's winCount on click of head-to-head player.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlayerList from '../PlayerList/PlayerList';
+import Game from '../Game/Game';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: {}
+      players: {},
+      player1: null,
+      player2: null
     }
 
     this.addPlayer = this.addPlayer.bind(this);
+    this.setPlayer = this.setPlayer.bind(this);
+    this.updatePlayer = this.updatePlayer.bind(this);
+    this.pickWinner = this.pickWinner.bind(this);
+    this.clearGame = this.clearGame.bind(this);
   }
 
   addPlayer(player) {
@@ -19,14 +26,55 @@ class App extends Component {
     this.setState({ players});
   }
 
+  setPlayer(key) {
+    // const player = this.state.players[key];
+    if(!this.state.player1) {
+      this.setState({ player1: key })
+    } else if (key != this.state.player1) {
+      this.setState({ player2: key })
+    } else {
+      alert("player1 can't be player2!");
+    }
+  }
+
+  updatePlayer(key, updatedPlayer) {
+    console.log("updatePlayer method!");
+    const players = {...this.state.players};
+    players[key] = updatedPlayer;
+    this.setState({ players });
+    this.clearGame();
+  }
+
+  pickWinner(player) {
+    console.log(`picking winner...`);
+  }
+
+  clearGame() {
+    this.setState({
+      player1: null,
+      player2: null
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Session</h1>
         <PlayerList
           players={this.state.players}
           addPlayer={this.addPlayer}
+          player1={this.state.player1}
+          player2={this.state.player2}
+          setPlayer={this.setPlayer}
         />
+        <Game
+          players={this.state.players}
+          player1={this.state.player1}
+          player2={this.state.player2}
+          updatePlayer={this.updatePlayer}
+          pickWinner={this.pickWinner}
+          clearGame={this.clearGame}
+        />
+        <h1>test</h1>
       </div>
     );
   }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import "./Game.css";
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.renderPlayer = this.renderPlayer.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    renderPlayer(key) {
+        if(!key) return;
+        const player = this.props.players[key];
+        return (
+            <div className="player-active" onClick={(e) => this.handleClick(key)}>
+                <img src={`/icons/${player.icon}`} className="player-icon" alt="logo" />
+                <h2 className="player-name">{player.name}</h2>
+            </div>
+        )
+    }
+
+    handleClick(key){
+        const player = this.props.players[key];
+        const updatedPlayer = {
+            ...player,
+            ["winCount"]: ++player.winCount
+        }
+        console.dir(updatedPlayer);
+        this.props.updatePlayer(key, updatedPlayer);
+    }
+
+    render() {
+        return (
+            <div className="Game">
+                <div className="players">
+                    {this.renderPlayer(this.props.player1)}
+                    {this.renderPlayer(this.props.player2)}
+                </div>
+                <button className="clear-game" onClick={this.props.clearGame}>Clear Game</button>
+            </div>
+        );
+    }
+}
+
+export default Game;
